a03ce269bb10f2b6efa6473546ff72bdd7e54fe6
luo hao <990747088@qq.com>
2018-04-16 17:59:13 +0800

    s3文件上传。

--- src/S3ObjectDownload/main.go
+++ src/S3ObjectDownload/main.go
@@ -6,31 +6,37 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		exitError("Bucket and item names required\nUsage: %s bucket_name item_name", os.Args[0])
-	}
+	bucket := "xl-pic"
+	item := "HappyFace.jpg"
 
-	bucket := os.Args[1]
-	item := os.Args[2]
-
-	file, err := os.Create(item)
+	file, err := os.Create(fmt.Sprintf("/Users/hao/Documents/Projects/Github/go_lambda_learning/src/S3PicResizeHandler/%s", item))
 	if nil != err {
 		exitError("unable to open file %q, %v", err)
 		return
 	}
 	defer file.Close()
 
-	fmt.Print(bucket)
-
 	ses, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
 	})
 	downloader := s3manager.NewDownloader(ses)
 
+	numBytes, err := downloader.Download(file, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(item),
+	})
+
+	if nil != err {
+		exitError("Unable to download item %q, %v", item, err)
+		return
+	}
+
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
 }
 
 func exitError(msg string, args ...interface{}) {
