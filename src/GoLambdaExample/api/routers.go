5dcab165a2dfefc18a677cd248120d9d55f40b87
luo hao <990747088@qq.com>
2018-04-20 00:12:55 +0800

    生成api routers

--- src/GoLambdaExample/api/routers.go
+++ src/GoLambdaExample/api/routers.go
@@ -1,13 +1,15 @@
 package api
 
 import (
+	"GoLambdaExample/api/info"
 	"GoLambdaExample/api/metric"
 
 	"github.com/gin-gonic/gin"
 )
 
 func BindRouters(engine *gin.Engine) (err error) {
+	engine.Handle("GET", "/info/hello", info.HelloFunc.HandlerFunc)
 	engine.Handle("GET", "/metric/hello", metric.HelloFunc.HandlerFunc)
-	// TODO add-new-handle
+	engine.Handle("POST", "/metric/hello_world", metric.HelloWorldFunc.HandlerFunc)
 	return
 }
