ee0d8fb849d6481d3b15dd178f6ce7a6b82471f5
luo hao <990747088@qq.com>
2018-05-10 16:22:52 +0800

    modify deploy.sh stage arg

--- src/ExampleSAMLocal/deploy.sh
+++ src/ExampleSAMLocal/deploy.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+
+stage=$1
+
+if [ -z ${stage} ]
+then
+    stage="dev"
+fi
+
+echo deploy target stage environment: ${stage}
+
 echo building...
-lbuild compile func
+lbuild compile func -s ${stage}
 
 echo deploying...
-ldeploy remote func
+ldeploy remote func -s ${stage}
--- src/ExampleSAMLocal/run.sh
+++ src/ExampleSAMLocal/run.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-echo building...
-lbuild compile func
-
 echo running main
 go build -o main main.go
 ./main
